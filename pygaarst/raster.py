72896dc95ffff0532d2e8e4d1c3dc8c84b69702d
Chris Waigl <chris.waigl@gmail.com>
2013-11-18 09:51:23 -0900

    First commit: Landsat support.

--- pygaarst/raster.py
+++ pygaarst/raster.py
@@ -3,6 +3,10 @@
 pygaarst.raster
 
 Classes and methods to handle raster file formats.
+Implemented:
+- GeoTIFF
+- HDF5 (stub)
+- Landsatband(GeoTIFF)
 
 Created by Chris Waigl on 2013-09-18.
 """
@@ -12,7 +16,6 @@ import numpy as np
 
 import h5py
 from osgeo import gdal, osr
-from osgeo import osr
 from pyproj import Proj
 from netCDF4 import Dataset as netCDF
 
@@ -36,9 +39,79 @@ class GeoTIFF(object):
         except RuntimeError as e: 
             logging.error("Could not open %s: %s" % (filepath, e))
             raise
+        self.filepath = filepath
         self.ncol = self.dataobj.RasterXSize
         self.nrow = self.dataobj.RasterYSize
+        self._gtr = self.dataobj.GetGeoTransform()
+        # see http://www.gdal.org/gdal_datamodel.html
+        self.ulx = self._gtr[0]
+        self.uly = self._gtr[3]
+        self.lrx = self.ulx + self.ncol * self._gtr[1] + self.nrow * self._gtr[2]
+        self.lry = self.uly + self.ncol * self._gtr[4] + self.nrow * self._gtr[5]
+    
+    @property
+    def data(self):
+        return self.dataobj.ReadAsArray()
+
+    @property
+    def projection(self):
+        return self.dataobj.GetProjection()
+
+    @property
+    def proj4(self):
+        osrref = osr.SpatialReference()
+        osrref.ImportFromWkt(self.projection)
+        return osrref.ExportToProj4()
+
+    def simpleplot(self):
+        import matplotlib.pyplot as plt
+        numbands = self.data.shape[0]
+        for idx in range(numbands):
+            fig = plt.figure()
+            plt.imshow(self.data[idx,:,:], cmap='bone')
+
+
+class Landsatscene(object):
+    """
+    A container object for TM/ETM+ L5/7 and OLI/TIRS L8 scenes. Input: directory name, 
+    which is expected to contain all scene files.     
+    """
+    SPACECRAFTS = {
+        'Landsat5': 'L5',
+        'LANDSAT_5': 'L5',
+        'LANDSAT_7': 'L7',
+        'LANDSAT_8': 'L8'
+    }
+    import landatutils as l
+    def __init__(self, dirname):
+        try:
+            logging.info("Loading Landsat scene %s" % dirname)
+        except RuntimeError as e: 
+            logging.error("Could not open %s: %s" % (dirname, e))
+            raise
+        self.location = dirname
+        self.meta = l.parsemeta(dirname)
+        # first of all, find out software version and satellite 
         
+
+class Landsatband(GeoTIFF):
+    """
+    Represents a band of a Landsat scene. 
+    
+    Implemented: v. 11 and 12 of the processing software for TM/ETM+ L5/7 and OLI/TIRS L8
+    """
+    import landatutils as l
+    
+    @property
+    def meta(self):
+        return l.parsemeta(os.path.basename(self.filepath)
+    
+    
+    
+class NetCDF(objectc):
+    pass
+
+
 class HDF5(object):
     """
     A class providing access to a HDF file or dataset
@@ -49,6 +122,7 @@ class HDF5(object):
     """
     import h5py
     def __init__(self, filepath, geofilepath=None, variable=None):
+        self.geofilepath = geofilepath
         try:
             logging.info("Opening %s" % filepath)
             self.dataobj = h5py.File(filepath, "r")
@@ -56,5 +130,12 @@ class HDF5(object):
             logging.error("Could not open %s: %s" % (filepath, e))
             raise
         self.bandname = self.dataobj['All_Data'].keys()[0]
-            
+        self.datasets = self.dataobj['All_Data/'+self.bandname].items()
+        
+    @property
+    def geodetics(self):
+        try:
+            self.geodataobj = h5py.File(self.geofilepath, "r")
+        except Exception as e:
+            print e
         
---
+++ tests/test_landsatutils.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+test_raster.py
+
+Created by Chris Waigl on 2013-11-10.
+"""
+
+import os, os.path
+import pytest
+from pygaarst import landsatutils as l
+
+def setup_module(module):
+    mydir = os.getcwd()
+    datapath8 = os.path.join(mydir, "tests/data/LC80690152013153LGN00_MTL.txt")
+    datapath51 = os.path.join(mydir, "tests/data/LT50690152011132GLC00_MTL.txt")
+    datapath52 = os.path.join(mydir, "tests/data/L5070015_01520090716_MTL.txt")
+    datapath7 = os.path.join(mydir, "tests/data/LE70700152011147EDC00_MTL.txt")
+    global datapaths
+    datapaths = [datapath8, datapath7, datapath51, datapath52]
+
+def test_file():
+    for p in datapaths:
+        a = os.path.isfile(p)
+        assert a
+
+def test_read_metadata_L8():
+    meta = l.parsemeta(datapaths[0])
+    assert meta['L1_METADATA_FILE']['PRODUCT_METADATA']['SPACECRAFT_ID']  == 'LANDSAT_8'
+    assert meta['L1_METADATA_FILE']['METADATA_FILE_INFO']['PROCESSING_SOFTWARE_VERSION'] == 'LPGS_2.2.2'
+
+
+def test_read_metadata_L7():
+    meta = l.parsemeta(datapaths[1])
+    assert meta['L1_METADATA_FILE']['PRODUCT_METADATA']['SPACECRAFT_ID']  == 'LANDSAT_7'
+    assert meta['L1_METADATA_FILE']['METADATA_FILE_INFO']['PROCESSING_SOFTWARE_VERSION'] == 'LPGS_12.2.1'
+
+def test_read_metadata_L51():
+    meta = l.parsemeta(datapaths[2])
+    assert meta['L1_METADATA_FILE']['PRODUCT_METADATA']['SPACECRAFT_ID']  == 'LANDSAT_5'
+    assert meta['L1_METADATA_FILE']['METADATA_FILE_INFO']['PROCESSING_SOFTWARE_VERSION'] == 'LPGS_12.2.1'
+
+def test_read_metadata_L52():
+    meta = l.parsemeta(datapaths[3])
+    assert meta['L1_METADATA_FILE']['PRODUCT_METADATA']['SPACECRAFT_ID']  == 'Landsat5'
+    assert meta['L1_METADATA_FILE']['PRODUCT_METADATA']['PROCESSING_SOFTWARE'] == 'LPGS_11.5.1'
+
