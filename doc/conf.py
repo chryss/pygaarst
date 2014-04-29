2374343db2c683c4cb094bdb70cda4233e54c909
Chris Waigl <chris.waigl@gi.alaska.edu>
2014-04-29 14:23:13 -0800

    Minor change for ReadTheDocs theming. #9

--- doc/conf.py
+++ doc/conf.py
@@ -103,6 +103,11 @@ pygments_style = 'default'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+if on_rtd:
+    html_theme = 'default'
+else:
+    html_theme = 'nature'
 html_theme = 'nature'
 
 # Theme options are theme-specific and customize the look and feel of a theme
