c3679f1c4064789a2d76bac5756e192d2832b6a3
Chris Waigl <chris.waigl@gmail.com>
2016-03-08 13:44:21 -0900

    add begin/end_object to ODL parser, refactor to parse strings, add tests

--- pygaarst/mtlutils.py
+++ pygaarst/mtlutils.py
@@ -16,6 +16,9 @@ from __future__ import division, print_function, absolute_import
 import os.path, glob
 import datetime
 import re
+from StringIO import StringIO
+from ast import literal_eval
+from collections import OrderedDict
 import logging
 logging.basicConfig(level=logging.DEBUG)
 LOGGER = logging.getLogger('pygaarst.mtlutils')
@@ -44,10 +47,27 @@ LOGGER = logging.getLogger('pygaarst.mtlutils')
 METAPATTERN = "*_MTL*"
 
 # Elements from the file format used for parsing
-GRPSTART = "GROUP = "
-GRPEND = "END_GROUP = "
-ASSIGNCHAR = " = "
+GRPSTART = "GROUP="
+GRPEND = "END_GROUP="
+OBJSTART = "OBJECT="
+OBJEND = "END_OBJECT="
+ASSIGNCHAR = "="
 FINAL = "END"
+IGNOREGROUPS = [
+    "INFORMATIONCONTENT"
+]
+IGNOREOBJECTS = [
+    "ADDITIONALATTRIBUTESCONTAINER",
+    "ADDITIONALATTRIBUTENAME",
+    "PARAMETERVALUE"
+]
+
+# Patterns for line sanitization
+SANITIZEPAT = [
+    (r'\s+=\s+', '='),
+    (r'^\s+', ''),
+    (r'\s+$', ''),
+]
 
 # A state machine is used to parse the file. There are 5 states (0 to 4):
 STATUSCODE = [
@@ -62,6 +82,23 @@ STATUSCODE = [
 class MTLParseError(Exception):
     """Custom exception: parse errors in Landsat or EO-1 MTL metadata files"""
     pass
+
+def _deep_convert_dict(layer):
+    """Helper function to convert dictionary back from OrderedDict"""
+    to_ret = layer
+    if isinstance(layer, OrderedDict):
+        to_ret = dict(layer)
+    try:
+        for key, value in to_ret.items():
+            to_ret[key] = _deep_convert_dict(value)
+    except AttributeError:
+        pass
+    return to_ret
+
+def _sanitizeline(line):
+    for pattern, repl in SANITIZEPAT:
+        line = re.sub(pattern, repl, line)
+    return line
 
 # Help functions to identify the current line and extract information
 def _islinetype(line, testchar):
@@ -84,6 +121,15 @@ def _getendgroupname(line):
     """Returns group name, if used with group end lines"""
     return line.strip().split(GRPEND)[-1]
 
+def _getobjname(line):
+    """Returns object name, if used with object start lines"""
+    return line.strip().split(OBJSTART)[-1]
+
+def _getendobjname(line):
+    """Returns group name, if used with group end lines"""
+    return line.strip().split(OBJEND)[-1]
+
+
 def _getmetadataitem(line):
     """Returns key/value pair for assignment type lines"""
     return line.strip().split(ASSIGNCHAR)
@@ -94,14 +140,18 @@ def _checkstatus(status, line):
     """Returns state/status after reading the next line.
 
     The status codes are::
-        0 - BEGIN parsing; 1 - ENTER METADATA GROUP, 2 - READ METADATA LINE,
-        3 - END METDADATA GROUP, 4 - END PARSING
+        0 - BEGIN parsing; 1 - ENTER METADATA GROUP, 2 - READ KEY-VALUE PAIR,
+        3 - END METDADATA GROUP, 4 - END PARSING, 5 - BEGIN OBJECT, 
+        6 - END OBJECT
 
     Permitted Transitions::
         0 --> 1, 0 --> 4
-        1 --> 1, 1 --> 2, 1 --> 3
-        2 --> 2, 2 --> 3
-        3 --> 1, 1 --> 3, 3 --> 4
+        1 --> 1, 1 --> 2, 1 --> 3, 1 --> 5
+        2 --> 1, 2 --> 2, 2 --> 3, 2 --> 5, 3 --> 6
+        3 --> 1, 3 --> 2, 3 --> 4, 3 --> 5, 3 --> 6
+        5 --> 1, 5 --> 2, 5 --> 5, 5 --> 6
+        6 --> 1, 6 --> 2, 6 --> 5, 6 --> 6
+        
     """
     newstatus = 0
     if status == 0:
@@ -117,12 +167,20 @@ def _checkstatus(status, line):
             newstatus = 1
         elif _islinetype(line, GRPEND):
             newstatus = 3
+        elif _islinetype(line, OBJSTART):
+            newstatus = 5
         elif _isassignment(line):
             # test AFTER start and end, as both are also assignments
             newstatus = 2
     elif status == 2:
         if _islinetype(line, GRPEND):
             newstatus = 3
+        elif _islinetype(line, GRPSTART):
+            newstatus = 1
+        elif _islinetype(line, OBJSTART):
+            newstatus = 5
+        elif _islinetype(line, OBJEND):
+            newstatus = 6
         elif _isassignment(line):
             # test AFTER start and end, as both are also assignments
             newstatus = 2
@@ -133,6 +191,37 @@ def _checkstatus(status, line):
             newstatus = 3
         elif _isfinal(line):
             newstatus = 4
+        elif _islinetype(line, OBJSTART):
+            newstatus = 5
+        elif _islinetype(line, OBJEND):
+            newstatus = 6
+        elif _isassignment(line):
+            # test AFTER start and end, as both are also assignments
+            newstatus = 2
+    elif status == 5:
+        if _islinetype(line, GRPSTART):
+            newstatus = 1
+        elif _islinetype(line, GRPEND):
+            newstatus = 3
+        elif _islinetype(line, OBJSTART):
+            newstatus = 5
+        elif _islinetype(line, OBJEND):
+            newstatus = 6
+        elif _isassignment(line):
+            # test AFTER start and end, as both are also assignments
+            newstatus = 2
+    elif status == 6:
+        if _islinetype(line, GRPSTART):
+            newstatus = 1
+        elif _islinetype(line, GRPEND):
+            newstatus = 3
+        elif _islinetype(line, OBJSTART):
+            newstatus = 5
+        elif _islinetype(line, OBJEND):
+            newstatus = 6
+        elif _isassignment(line):
+            # test AFTER start and end, as both are also assignments
+            newstatus = 2
     if newstatus != 0:
         return newstatus
     elif status != 4:
@@ -140,7 +229,7 @@ def _checkstatus(status, line):
             "Cannot parse the following line after status "
             + "'%s':\n%s" % (STATUSCODE[status], line))
 
-# Function to execute when reading a line in a given state
+
 def _transstat(status, grouppath, dictpath, line):
     """Executes processing steps when reading a line"""
     if status == 0:
@@ -150,29 +239,57 @@ def _transstat(status, grouppath, dictpath, line):
     elif status == 1:
         currentdict = dictpath[-1]
         currentgroup = _getgroupname(line)
-        grouppath.append(currentgroup)
-        currentdict[currentgroup] = {}
-        dictpath.append(currentdict[currentgroup])
+        grouppath.append((currentgroup, 'g'))
+        if currentgroup not in IGNOREGROUPS:
+            currentdict[currentgroup] = OrderedDict()
+            dictpath.append(currentdict[currentgroup])
     elif status == 2:
         currentdict = dictpath[-1]
+        currentparent = grouppath[-1]
         newkey, newval = _getmetadataitem(line)
-        currentdict[newkey] = _postprocess(newval)
+        if currentparent[1] == 'g':
+            if currentparent[0] not in IGNOREGROUPS:
+                currentdict[newkey] = _postprocess(newval)
+        elif currentparent[1] == 'o':
+            if newkey == 'VALUE':
+                if currentparent[0] == "ADDITIONALATTRIBUTENAME":
+                    currentdict[_postprocess(newval)] = None
+                elif currentparent[0] == "PARAMETERVALUE":
+                    curkey, curval = currentdict.popitem()
+                    currentdict[curkey] = _postprocess(newval)
+                else:
+                    currentdict[currentparent[0]] = _postprocess(newval)
     elif status == 3:
         oldgroup = _getendgroupname(line)
-        if oldgroup != grouppath[-1]:
+        if oldgroup != grouppath[-1][0]:
             raise MTLParseError(
                 "Reached line '%s' while reading group '%s'."
-                % (line.strip(), grouppath[-1]))
+                % (line.strip(), grouppath[-1][0]))
         del grouppath[-1]
-        del dictpath[-1]
+        if oldgroup not in IGNOREGROUPS:
+            del dictpath[-1]
         try:
-            currentgroup = grouppath[-1]
+            currentgroup = grouppath[-1][0]
         except IndexError:
             currentgroup = None
     elif status == 4:
         if grouppath:
             raise MTLParseError(
                 "Reached end before end of group '%s'" % grouppath[-1])
+    elif status == 5:
+        currentobj = _getobjname(line)
+        grouppath.append((currentobj, 'o'))
+    elif status == 6:
+        oldobj = _getendobjname(line)
+        if oldobj != grouppath[-1][0]:
+            raise MTLParseError(
+                "Reached line '%s' while reading object '%s'."
+                % (line.strip(), grouppath[-1][0]))
+        del grouppath[-1]
+        try:
+            currentobj = grouppath[-1][0]
+        except IndexError:
+            currentobj = None
     return grouppath, dictpath
 
 # Identifying data type of a metadata item and
@@ -180,40 +297,49 @@ def _postprocess(valuestr):
     """
     Takes value as str, returns str, int, float, date, datetime, or time
     """
-    intpattern = re.compile(r'^\-?\d+$')
-    floatpattern = re.compile(r'^\-?\d+\.\d+(E[+-]?\d\d+)?$')
+    intpattern = re.compile(r'^"?\-?\d+"?$')
+    floatpattern = re.compile(r'^"?\-?\d+\.\d+(E[+-]?\d\d+)?"?$')
     datedtpattern = '%Y-%m-%d'
     datedttimepattern = '%Y-%m-%dT%H:%M:%SZ'
     timedtpattern = '%H:%M:%S.%f'
-    timepattern = re.compile(r'^\d{2}:\d{2}:\d{2}(\.\d{6})?')
-    if valuestr.startswith('"') and valuestr.endswith('"'):
-        # it's a string
-        return valuestr[1:-1]
-    elif re.match(intpattern, valuestr):
+    timepattern = re.compile(r'^"?\d{2}:\d{2}:\d{2}(\.\d{6})?"?')
+
+    # can we automatically parse it into something numeric?
+    teststr = valuestr.replace("'", "").replace('"', "")
+    try:
+        return literal_eval(teststr.strip())
+    except ValueError:
+        pass   # try something else
+    except SyntaxError:
+        pass
+    if re.match(intpattern, teststr):
         # it's an integer
-        return int(valuestr)
-    elif re.match(floatpattern, valuestr):
+        return int(teststr)
+    elif re.match(floatpattern, teststr):
         # floating point number
-        return float(valuestr)
+        return float(teststr)
     # now let's try the datetime objects; throws exception if it doesn't match
     try:
-        return datetime.datetime.strptime(valuestr, datedtpattern).date()
+        return datetime.datetime.strptime(teststr, datedtpattern).date()
     except ValueError:
         pass
     try:
-        return datetime.datetime.strptime(valuestr, datedttimepattern)
+        return datetime.datetime.strptime(teststr, datedttimepattern)
     except ValueError:
         pass
     # time parsing is complicated: Python's datetime module only accepts
     # fractions of a second only up to 6 digits
-    mat = re.match(timepattern, valuestr)
+    mat = re.match(timepattern, teststr)
     if mat:
         test = mat.group(0)
         try:
             return datetime.datetime.strptime(test, timedtpattern).time()
         except ValueError:
             pass
-
+    if valuestr.startswith('"') and valuestr.endswith('"'):
+        # it's a string
+            return valuestr[1:-1]
+    
     # If we get here, we still haven't returned anything.
     logging.info(
         "The value %s couldn't be parsed as " % valuestr
@@ -224,7 +350,8 @@ def parsemeta(metadataloc):
     """Parses the metadata.
 
     Arguments:
-        metadataloc: a filename or a directory.
+        metadataloc: a filename or a directory. Or a the string to 
+        be parsed as an ODL block
     Returns metadata dictionary
     """
 
@@ -236,33 +363,45 @@ def parsemeta(metadataloc):
                 "No files matching metadata file pattern in directory %s."
                 % metadataloc)
         elif len(metalist) > 0:
-            metadatafn = metalist[0]
+            metadatastr = metalist[0]
             if len(metalist) > 1:
                 logging.warning(
                     "More than one file in directory match metadata "
                     + "file pattern. Using %s." % metadatafn)
-    elif os.path.isfile(metadataloc):
-        metadatafn = metadataloc
-        logging.info("Using file %s." % metadatafn)
     else:
-        raise MTLParseError(
-            "File location %s is unavailable " % metadataloc
-            + "or doesn't contain a suitable metadata file.")
+        metadatastr = metadataloc
 
     # Reading file line by line and inserting data into metadata dictionary
+    try:
+        with open(metadatastr, 'rU') as filehandle:
+            metadata = _parsemetastream(filehandle)
+    except IOError:
+        # maybe the argument is a string. Converting to a file-like object
+        logging.info("It's not a file, trying to open it as a string.")
+        try:
+            inpstream = StringIO(metadatastr)
+        except:
+            raise MTLParseError(
+            "%s does not appear to be a file-like object " % metadatastr)
+        metadata = _parsemetastream(inpstream)
+    return _deep_convert_dict(metadata)
+
+def _parsemetastream(filehandle):
     status = 0
-    metadata = {}
+    metadata = OrderedDict()
     grouppath = []
     dictpath = [metadata]
-    with open(metadatafn, 'rU') as filehandle:
-        for line in filehandle:
-            if status == 4:
-                # we reached the end in the previous iteration,
-                # but are still reading lines
-                logging.warning(
-                    "Metadata file %s appears to " % metadatafn
-                    + "have extra lines after the end of the metadata. "
-                    + "This is probably, but not necessarily, harmless.")
-            status = _checkstatus(status, line)
-            grouppath, dictpath = _transstat(status, grouppath, dictpath, line)
+    for line in filehandle:
+        line = _sanitizeline(line)
+        if not line:
+            continue
+        if status == 4:
+            # we reached the end in the previous iteration,
+            # but are still reading lines
+            logging.warning(
+                "Metadata file %s appears to " % metadatafn
+                + "have extra lines after the end of the metadata. "
+                + "This is probably, but not necessarily, harmless.")
+        status = _checkstatus(status, line)
+        grouppath, dictpath = _transstat(status, grouppath, dictpath, line)
     return metadata
--- tests/test_mtlutils.py
+++ tests/test_mtlutils.py
@@ -12,6 +12,114 @@ import pytest
 from pygaarst import mtlutils as mtl
 
 DATADIR = "tests/data"
+MODL2meta = """
+
+GROUP                  = INVENTORYMETADATA
+  GROUPTYPE            = MASTERGROUP
+
+  GROUP                  = ECSDATAGRANULE
+
+    OBJECT                 = LOCALGRANULEID
+      NUM_VAL              = 1
+      VALUE                = "MOD021KM.A2015167.0805.005.2015170121435.hdf"
+    END_OBJECT             = LOCALGRANULEID
+
+    OBJECT                 = PRODUCTIONDATETIME
+      NUM_VAL              = 1
+      VALUE                = "2015-06-19T12:14:36.000Z"
+    END_OBJECT             = PRODUCTIONDATETIME
+
+    OBJECT                 = DAYNIGHTFLAG
+      NUM_VAL              = 1
+      VALUE                = "Both"
+    END_OBJECT             = DAYNIGHTFLAG
+
+    OBJECT                 = REPROCESSINGACTUAL
+      NUM_VAL              = 1
+      VALUE                = "processed once"
+    END_OBJECT             = REPROCESSINGACTUAL
+
+    OBJECT                 = REPROCESSINGPLANNED
+      NUM_VAL              = 1
+      VALUE                = "further update is anticipated"
+    END_OBJECT             = REPROCESSINGPLANNED
+
+  END_GROUP              = ECSDATAGRANULE
+
+  GROUP                  = ANCILLARYINPUTGRANULE
+
+    OBJECT                 = ANCILLARYINPUTGRANULECONTAINER
+      CLASS                = "1"
+
+      OBJECT                 = ANCILLARYINPUTPOINTER
+        CLASS                = "1"
+        NUM_VAL              = 1
+        VALUE                = "MOD03.A2015167.0805.005.2015170114131.hdf"
+      END_OBJECT             = ANCILLARYINPUTPOINTER
+
+      OBJECT                 = ANCILLARYINPUTTYPE
+        CLASS                = "1"
+        NUM_VAL              = 1
+        VALUE                = "Geolocation"
+      END_OBJECT             = ANCILLARYINPUTTYPE
+
+    END_OBJECT             = ANCILLARYINPUTGRANULECONTAINER
+
+  END_GROUP              = ANCILLARYINPUTGRANULE
+
+
+  GROUP                  = ADDITIONALATTRIBUTES
+
+    OBJECT                 = ADDITIONALATTRIBUTESCONTAINER
+      CLASS                = "1"
+
+      OBJECT                 = ADDITIONALATTRIBUTENAME
+        CLASS                = "1"
+        NUM_VAL              = 1
+        VALUE                = "AveragedBlackBodyTemperature"
+      END_OBJECT             = ADDITIONALATTRIBUTENAME
+
+      GROUP                  = INFORMATIONCONTENT
+        CLASS                = "1"
+
+        OBJECT                 = PARAMETERVALUE
+          NUM_VAL              = 1
+          CLASS                = "1"
+          VALUE                = " 290.01"
+        END_OBJECT             = PARAMETERVALUE
+
+      END_GROUP              = INFORMATIONCONTENT
+
+    END_OBJECT             = ADDITIONALATTRIBUTESCONTAINER
+
+    OBJECT                 = ADDITIONALATTRIBUTESCONTAINER
+      CLASS                = "2"
+
+      OBJECT                 = ADDITIONALATTRIBUTENAME
+        CLASS                = "2"
+        NUM_VAL              = 1
+        VALUE                = "AveragedMirrorTemperature"
+      END_OBJECT             = ADDITIONALATTRIBUTENAME
+
+      GROUP                  = INFORMATIONCONTENT
+        CLASS                = "2"
+
+        OBJECT                 = PARAMETERVALUE
+          NUM_VAL              = 1
+          CLASS                = "2"
+          VALUE                = " 277.85"
+        END_OBJECT             = PARAMETERVALUE
+
+      END_GROUP              = INFORMATIONCONTENT
+
+    END_OBJECT             = ADDITIONALATTRIBUTESCONTAINER
+
+   END_GROUP              = ADDITIONALATTRIBUTES
+
+END_GROUP              = INVENTORYMETADATA
+
+END
+"""
 
 def setup_module(module):
     mydir = DATADIR
@@ -31,3 +139,9 @@ def test_read_metadata_L8():
     meta = mtl.parsemeta(metadatapaths[0])
     assert meta['L1_METADATA_FILE']['PRODUCT_METADATA']['SPACECRAFT_ID']  == 'LANDSAT_8'
     assert meta['L1_METADATA_FILE']['METADATA_FILE_INFO']['PROCESSING_SOFTWARE_VERSION'] == 'LPGS_2.2.2'
+
+def test_read_MODL2_metadata_from_string():
+    meta = mtl.parsemeta(MODL2meta)
+    assert meta["INVENTORYMETADATA"]["ANCILLARYINPUTGRANULE"]["ANCILLARYINPUTPOINTER"] == 'MOD03.A2015167.0805.005.2015170114131.hdf'
+    assert meta["INVENTORYMETADATA"]["ADDITIONALATTRIBUTES"]["AveragedBlackBodyTemperature"] == 290.01
+    assert len(meta["INVENTORYMETADATA"]) == 4
