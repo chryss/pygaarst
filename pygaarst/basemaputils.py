a85e259f6d1fe5b5aa195cbd40b6f28b14211da5
Chris Waigl <chris.waigl@gmail.com>
2019-07-18 12:52:55 -0800

    Added functionality so that maptransform can handle multi-type geometry collections (multipolygon etc.)

--- pygaarst/basemaputils.py
+++ pygaarst/basemaputils.py
@@ -45,13 +45,35 @@ def map_interiorAK(
     bmap.drawmapboundary(fill_color=water)
     return bmap
 
+def extractLons(listoftups):
+    """Given a sequence of tuples the first two elements of which are
+    longitude and latitude, return list of lons"""
+    return [item[0] for item in listoftups]
+
+def extractLats(listoftups):
+    """Given a sequence of tuples the first two elements of which are
+    longitude and latitude, return list of lons"""
+    return [item[1] for item in listoftups]
 
 def maptransform(mmap, record):
     """Given a Basemap object and a Fiona collection record in geographic
     coordinates, return a new record whose coordinates are transformed into
     the map CRS"""
-    lons = [item[0] for item in record['geometry']['coordinates'][0]]
-    lats = [item[1] for item in record['geometry']['coordinates'][0]]
-    record['geometry']['coordinates'] = [[(x, y)
-        for (x, y) in zip(mmap(lons, lats)[0], mmap(lons, lats)[1])]]
+    if record['geometry']['type'].lower() in ['polygon', 'point', 'linestring']:
+        print("record type is {}".format(record['geometry']['type']))
+        record['geometry']['coordinates'] = [
+            list(zip(*mmap(extractLons(listoftups), extractLats(listoftups))))
+            for listoftups in record['geometry']['coordinates']
+            ]
+    elif record['geometry']['type'].lower() in ['multipolygon', 'multipoint', 'multilinestring']:
+        print("record type is {}".format(record['geometry']['type']))
+        record['geometry']['coordinates'] = [
+            [
+                list(zip(*mmap(extractLons(listoftups), extractLats(listoftups))))
+                for listoftups in recorditem
+            ]
+            for recorditem in record['geometry']['coordinates'][0]
+            ]
+    else:
+        raise NotImplementedError("Unkown record type {}".format(record['geometry']['type']))
     return record
